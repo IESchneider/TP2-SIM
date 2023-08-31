3639205ead33d89c74a348b6f277ce2fe92a2feb
schneider85459 <85459@sistemas.frc.utn.edu.ar>
2023-08-31 16:14:26 -0300

    inicio

---
+++ Distribuciones/Exponencial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2_SIM.Distribuciones
+{
+    internal class Exponencial
+    {
+        public int CantidadMuestra { get; set; }
+        public int CantidadIntervalos { get; set; }
+        public double Lambda { get; set; }
+        
+    }
+}
